fa8381ec990ba37d3d01282f2bde198ebdad0eac
Mohamed-HAZAMI <med.hazami@gmail.com>
2023-04-09 15:16:33 +0100

    add comp1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MyComponent from './comp';
+import MyComponent1 from './comp1';
 
 function App() {
   return (
     <div className="App">
        <div>hello master</div>
        <MyComponent/>
+       <MyComponent1/>
     </div>
   );
 }
