24c83da33077d00827d668fb87de71c8bf8fa8ee
Mohamed-HAZAMI <med.hazami@gmail.com>
2023-04-09 15:26:03 +0100

    strash push

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MyComponent from './comp';
 function App() {
   return (
     <div className="App">
+      <div>hi main</div>
        <div>hello master</div>
        <MyComponent/>
     </div>
