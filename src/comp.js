1d9e3d16b6414b68de6fcd35f2918e3d654128b9
Mohamed-HAZAMI <med.hazami@gmail.com>
2023-04-09 15:14:08 +0100

    add hi

--- src/comp.js
+++ src/comp.js
@@ -7,6 +7,7 @@ function MyComponent() {
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>hi</h1>
     </div>
   );
 }
